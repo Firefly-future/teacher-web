190162e73d18d511653f58e521dbb17e8cdf5710
han <3193873553@qq.com>
2025-12-23 17:29:25 +0800

    添加token以及验证

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,27 +5,19 @@ import RoleList from "@/pages/role/RoleList"
 import UserList from "@/pages/users/UserList"
 import Permission from "@/pages/permission/Permission"
 import NotFound from "@/pages/404/NotFound"
+
+import Auth from "./Auth"
+
 const router = [
-  { path: "/", element: <Home /> },
+  { path: "/", element: <Auth> <Home /> </Auth>, children: [
+    { path: "/dashboard", element: <DashBoard /> },
+    { path: "/roles", element: <RoleList /> },
+    { path: "/users", element: <UserList /> },
+    { path: "/permission", element: <Permission /> },
+  ] },
   {
     path: "/login",
     element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: <DashBoard />,
-  },
-  {
-    path: "/roles",
-    element: <RoleList />,
-  },
-  {
-    path: "/users",
-    element: <UserList />,
-  },
-  {
-    path: "/permission",
-    element: <Permission />,
   },
   {
     path: "*",
