249586655d3cbe0edfff1bdc7da06f836014a27f
han <3193873553@qq.com>
2025-12-26 08:01:42 +0800

    添加service

--- src/services/index.ts
+++ src/services/index.ts
@@ -50,8 +50,30 @@ export const createUser = (params: CreateUserParams) => {
 export const updateUser = (params: UpdateUserParams) => {
   return post<BaseResponse>('/user/update', params)
 }
-
+// 获取角色列表
 export const getRoleList = () => {
   return get<BaseResponse<RoleItemRes[]>>('/role/list')
 }
 
+// 编辑角色
+export const updateRole = (params: UpdateRoleParams) =>{
+  return post<BaseResponse>('/role/update', params)
+}
+
+// 获取权限列表
+export const getPermissionList = () =>{
+  return get<BaseResponse<PermissionListRes>>('/permission/list')
+}
+
+// 创建菜单
+export const createMenu = (params: CreateMenuParams) =>{
+  return post<BaseResponse>('/permission/create', params)
+}
+// 删除菜单
+export const deleteMenu = (id: string) =>{
+  return post<BaseResponse>('/permission/remove', {id})
+}
+// 更新菜单
+export const updateMenu = (id:string) =>{
+  return post<BaseResponse>('/permission/update', {id})
+}
